e1e78986440c1aec0cc053e9d2cd11abbc815a0e
Mp <parsasheini87@gmail.com>
2024-03-03 17:52:16 +0330

    Finished Part 5... ✔

--- 7 - Data Collection/Project/Project.py
+++ 7 - Data Collection/Project/Project.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 
-def get_json(json_data, city_kw): 
+def get_json(city_kw):
     return requests.post(f'https://gw.jabama.com/api/v4/keyword/{ city_kw }', json=json_data).json()
 
 
@@ -11,7 +11,8 @@ def set_max(accomm_dict):
     for title in scores_max: scores_max[title] = max(scores_max[title], accomm_dict[title])
 
 
-def get_accommodation_dict(accomm, accomm_dict = {}): # Structure of keys : (our_dict_key_name, api_parent_key_name, api_key_name) or (key_name, parent_key_name)
+def get_accommodation_dict(accomm): # Structure of keys : (our_dict_key_name, api_parent_key_name, api_key_name) or (key_name, parent_key_name)
+    accomm_dict = {}
     keys_list = [('areaSize', 'accommodationMetrics'), ('bathroomsCount', 'accommodationMetrics'), ('bedroomsCount', 'accommodationMetrics'), ('buildingSize', 'accommodationMetrics'), ('iranianToiletsCount', 'accommodationMetrics'), ('toiletsCount', 'accommodationMetrics'), ('name', None), ('kind', None), ('min_night', None), ('min_price', None), ('payment_type', None), ('region', None), ('reservation_type', None), ('city', 'location'), ('province', 'location'), ('geo', 'location'), ('rate_count', 'rate_review', 'count'), ('rate_score', 'rate_review', 'score'), ('type', None), ('status', None), ('verified', None), ('mainPrice', 'price'), ('discountedPrice', 'price'), ('price_perNight', 'price', 'perNight'), ('placeType', None), ('capacity_base', 'capacity', 'base'), ('capacity_extra', 'capacity', 'extra')]
     for key in keys_list : accomm_dict[ key[0] ] = accomm[ key[1] ][ key[2] ] if len(key) == 3 else accomm[ key[1] ][ key[0] ] if key[1] else accomm[ key[0] ]
 
@@ -19,6 +20,7 @@ def get_accommodation_dict(accomm, accomm_dict = {}): # Structure of keys : (our
     accomm_dict['url'] = f"https://www.jabama.com/stay/{ accomm_dict['type'] }-{ accomm['code'] }"
 
     set_max( accomm_dict )
+
     return accomm_dict
 
 
@@ -26,8 +28,8 @@ def write_data(accomm_list):
     with open(f'accommodations_data.json', 'w') as f : json.dump(accomm_list, f)
 
 
-def col_city(city_kw, json_data):
-    data, accommodations_list = get_json(json_data, city_kw=city_kw), []
+def col_city(city_kw):
+    data, accommodations_list = get_json(city_kw), []
     total = data['result']['total']
 
     while data['result']['items']:
@@ -39,24 +41,25 @@ def col_city(city_kw, json_data):
         print(f'Collected { len( accommodations_list ) } / { total }')
 
         json_data['page-number'] += 1
-        data = get_json(json_data, city_kw=city_kw)
+        data = get_json(city_kw)
 
     return accommodations_list
 
 
-def col_all(json_data):
+def col_all():
     accom_list = []
     for city in cities:
         accom_list += col_city(city, json_data)
-        json_data['page-num']
+        json_data['page-num'] = 1
 
     write_data( accom_list )
 
 
 def calc_score(accomm, title):
-    title_data, is_related = inputs['secondary inputs'][title], lambda x: x in accomm['amentities'] if title == 'amentities' else x == accomm['type']
+    title_data = inputs['secondary inputs'][title] # is_related = lambda x: x in accomm['amenities'] if title == 'amenities' else x == accomm['type']
     if type(title_data) != list : return title_data['importance'] * ( title_data['value'] - accomm[title] ) / scores_max[title]
-    else : return sum([ 0 if is_related( item['value'] ) else item['importance'] for item in title_data ])
+    elif title == 'amenities' : return sum([ 0 if item['value'] in accomm['amenities'] else item['importance'] for item in title_data ])
+    else : return sum([ 0 if item['value'] == accomm['type'] else item['importance'] for item in title_data ])
 
 
 def get_scores(accomm):
@@ -70,18 +73,26 @@ def init():
     scores_max = {'price_perNight': 0, 'toiletsCount': 0, 'bedroomsCount': 0}
     inputs = {
         'primary inputs': {
-            'city': input(),
-            'start_date': input(),
-            'end_date': input(),
-            'capacity': int(input())
+            'city': 'city-ramsar',
+            'start_date': 20240303,
+            'end_date': 20240304,
+            'capacity': 3
         },
 
         'secondary inputs': {
-            'bedroomsCount': {'value': 0, 'importance': 0},
-            'toiletsCount': {'value': 0, 'importance': 0},
-            'amentities': [ {'value': 0, 'importance': 0} ],
-            'types': [ {'value': 0, 'importance': 0} ],
-            'price': {'value': 0, 'importance': 0}
+            'bedroomsCount': {'value': 2, 'importance': 70},
+            'toiletsCount': {'value': 1, 'importance': 90},
+            'amenities': [
+                {'value': 'water', 'importance': 70},
+                {'value': 'cabinet', 'importance': 70}
+            ],
+
+            'types': [
+                {'value': 'villa', 'importance': 70},
+                {'value': 'apartment', 'importance': 40}
+            ],
+
+            'price_perNight': {'value': 4500000, 'importance': 70}
         }
     }
 
@@ -91,17 +102,18 @@ def init():
         'page-number': 1,
         'date': {
             'start': inputs['primary inputs']['start_date'],
-            'end': inputs['primary inputs']['start_date'],
+            'end': inputs['primary inputs']['end_date'],
         },
     }
 
 
 def main():
     init()
-    cities_data, scores = col_city(inputs['primary inputs']['city'], json_data), {}
+    cities_data, scores = col_city(inputs['primary inputs']['city']), {}
+
     for accomm in cities_data:
         scores[ accomm['url'] ] = get_scores(accomm)
 
-    return sorted(scores, key=lambda k: scores[k])
+    print( sorted(scores, key=lambda k: scores[k])[:10] )
 
 main()
